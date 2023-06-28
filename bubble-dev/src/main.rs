ded0304c11d4c8648cc2809900a3aa7d0e8cc154
Corentin Machu <corentin.machu@protonmail.com>
2023-06-28 12:25:29 +0200

    feat(type_system): Added basic renamer

--- bubble-dev/src/main.rs
+++ bubble-dev/src/main.rs
@@ -1,13 +1,13 @@
 use libbubble::{
     ast,
     parser::{grammar::GlobalStatementsParser, lexer::Lexer},
+    type_system,
 };
 
 fn main() {
     let lexer = Lexer::new(
         r#"
-    function f() { 42 }
-    function g(a: u32, b: bool) {
+    function g(a: u32, b: bool): i32 {
         let n = 32;
 
         while n > 21 {
@@ -16,14 +16,28 @@ fn main() {
 
         return n;
     }
-    function h(b: bool) { 42 }
+
+    function f(): u64 {
+        let n = 42;
+        return n;
+    }
 "#,
     );
     let parser = GlobalStatementsParser::new();
 
     match parser.parse(lexer) {
-        Ok(stmts) => {
+        Ok(mut stmts) => {
             let mut printer = ast::Printer::default();
+            let mut binder = type_system::binder::Binder::default();
+            let mut type_checker = type_system::type_checker::TypeChecker::default();
+            let mut renamer = type_system::Renamer::default();
+            binder.bind_statements(&mut stmts).expect("binder failed");
+            type_checker
+                .check_statements(&mut stmts)
+                .expect("type checker failed");
+            renamer
+                .rename_statements(&mut stmts)
+                .expect("renamer failed");
             printer.print(stmts).expect("write to stdio failed");
         }
         Err(err) => {
